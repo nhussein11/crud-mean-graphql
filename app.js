4708f6a3822ed77b0cb3269744f6caa39ee92d46
Nicolás Hussein <80422357+nhussein11@users.noreply.github.com>
2022-09-06 12:23:53 -0300

    add: app

---
+++ app.js
@@ -0,0 +1,34 @@
+const bodyParser = require("body-parser");
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const { graphqlHTTP } = require("express-graphql");
+const { graphql } = require("graphql");
+
+const graphqlSchema = require('./graphql/schema')
+const graphqlResolver = require('./graphql/resolvers')
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: graphqlSchema,
+    rootValue: graphqlResolver,
+    graphiql: true,
+  })
+);
+
+mongoose
+  .connect("mongodb://root:1234@localhost:27017/", {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  })
+  .then(() => {
+    app.listen(4000, console.log("Connectiong to port 3000"));
+  })
+  .catch((error) => console.error("MongoDB connection failed:", error.message))
+  
+console.log("Running a GraphQL API server at http://localhost:4000/graphql");
