08cf7f2e50973ec62361f49dac9dc8b7a1e81bbf
Nicolás Hussein <80422357+nhussein11@users.noreply.github.com>
2022-09-19 11:22:15 -0300

    fix: quote update resolver

--- graphql/resolvers/Quote.resolver.js
+++ graphql/resolvers/Quote.resolver.js
@@ -20,12 +20,16 @@ module.exports = {
     };
   },
   updateQuote: async ({ id, quoteInput }) => {
+    const {quote, author} = quoteInput
     const updatedQuote = await Quote.findByIdAndUpdate(id, {
-      quoteInput,
-    });
+      quote,
+      author
+    }, {new:true});
     if (!updatedQuote) {
       throw new Error("No quote found!");
     }
+    console.log(quoteInput)
+    console.log(updatedQuote)
     return {
       ...updatedQuote.toJSON(),
       _id: updatedQuote._id.toString(),
