ff8142cb9562f90b347b4083867ae4639791a769
Nicolás Hussein <80422357+nhussein11@users.noreply.github.com>
2022-09-26 11:56:48 -0300

    add: environments variables to the project

--- database/connection.js
+++ database/connection.js
@@ -1,11 +1,13 @@
 const mongoose = require("mongoose");
+require('dotenv').config()
+
 
 const connectToDatabase = async () => {
-    mongoose
-        .connect("mongodb://root:1234@localhost:27017/", {
-        useUnifiedTopology: true,
-        useNewUrlParser: true,
-        })
-}
+  mongoose.connect(`mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/`, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  });
+};
 
-module.exports = {connectToDatabase}
+module.exports = { connectToDatabase };
+ 
