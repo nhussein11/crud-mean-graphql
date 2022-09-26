0f660dd40f8d45a5453989fdd25193e09c53e614
Nicolás Hussein <80422357+nhussein11@users.noreply.github.com>
2022-09-26 09:27:22 -0300

    add: home-quote component

---
+++ src/app/components/home/home-quote/home-quote/home-quote.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-quote',
+  templateUrl: './home-quote.component.html',
+})
+export class HomeQuoteComponent {}
--- src/app/components/home/home.module.ts
+++ src/app/components/home/home.module.ts
@@ -2,11 +2,12 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
 import { LoginModule } from '../auth/login/login.module';
+import { HomeQuoteComponent } from './home-quote/home-quote/home-quote.component';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, HomeQuoteComponent],
   imports: [CommonModule, HomeRoutingModule, LoginModule],
   exports: [HomeComponent],
 })
