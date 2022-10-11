23e868036bea65190683b6dc43049b6f9a4e1abe
Nicolás Hussein <80422357+nhussein11@users.noreply.github.com>
2022-10-11 10:53:24 -0300

    fix: profile template logic using component logic

--- src/app/components/auth/welcome/welcome.component.ts
+++ src/app/components/auth/welcome/welcome.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import { Observable } from 'rxjs';
+import { NgClass } from '@angular/common';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Observable, Subscription } from 'rxjs';
 
 import { UserWithoutPassword } from 'src/app/shared/models/User';
 import { UserLoggedService } from 'src/app/shared/services/user-logged.service';
@@ -8,8 +9,30 @@ import { UserLoggedService } from 'src/app/shared/services/user-logged.service';
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
 })
-export class WelcomeComponent {
-  _userLogged$: Observable<UserWithoutPassword> =
-    this._userLoggedService.userLogged;
+export class WelcomeComponent implements OnInit, OnDestroy {
+  public title!: string;
+  public iconClass!: string;
+  public isUserLogged!: boolean;
+  private _userLogged$!: Observable<UserWithoutPassword>;
+  private _userLogged!: Subscription;
+
   constructor(private _userLoggedService: UserLoggedService) {}
+
+  ngOnInit(): void {
+    this._userLogged$ = this._userLoggedService.userLogged;
+    this._userLogged = this._userLogged$.subscribe(user => {
+      if (!user.name) {
+        this.title = 'Login';
+        this.iconClass = 'bi bi-door-open';
+        this.isUserLogged = false;
+      } else {
+        this.title = 'Welcome ' + user.name;
+        this.iconClass = 'bi bi-person-badge';
+        this.isUserLogged = true;
+      }
+    });
+  }
+  ngOnDestroy(): void {
+    this._userLogged.unsubscribe();
+  }
 }
