ebd6a842cddefcaa2a619eec0d4c89aa6ce5e92f
Nicolás Hussein <80422357+nhussein11@users.noreply.github.com>
2022-10-11 11:30:53 -0300

    add: UPDATE_PROFILE_MUTATION to profile component

--- src/app/components/auth/profile/profile.component.ts
+++ src/app/components/auth/profile/profile.component.ts
@@ -1,11 +1,24 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { gql } from 'apollo-angular';
 import { Subscription } from 'rxjs';
 
 import { UserWithoutPassword } from 'src/app/shared/models/User';
 import { TokenService } from 'src/app/shared/services/token.service';
 import { UserLoggedService } from 'src/app/shared/services/user-logged.service';
 
+const UPDATE_PROFILE_MUTATION = gql`
+  mutation ($_id: ID!, $name: String!, $address: String!, $email: String!) {
+    updateUser(
+      _id: $_id
+      userInput: { name: $name, address: $address, email: $email }
+    ) {
+      name
+      address
+      email
+    }
+  }
+`;
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
