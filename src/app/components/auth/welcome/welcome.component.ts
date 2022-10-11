ae7c3e0b9335a7532e2c6985e50889cc597ac2f2
Nicolás Hussein <80422357+nhussein11@users.noreply.github.com>
2022-10-11 10:56:50 -0300

    fix: deleting unnecesary observable declaration

--- src/app/components/auth/welcome/welcome.component.ts
+++ src/app/components/auth/welcome/welcome.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Observable, Subscription } from 'rxjs';
+import { Subscription } from 'rxjs';
 
-import { UserWithoutPassword } from 'src/app/shared/models/User';
 import { UserLoggedService } from 'src/app/shared/services/user-logged.service';
 
 @Component({
@@ -12,14 +11,12 @@ export class WelcomeComponent implements OnInit, OnDestroy {
   public title!: string;
   public iconClass!: string;
   public isUserLogged!: boolean;
-  private _userLogged$!: Observable<UserWithoutPassword>;
   private _userLogged!: Subscription;
 
   constructor(private _userLoggedService: UserLoggedService) {}
 
   ngOnInit(): void {
-    this._userLogged$ = this._userLoggedService.userLogged;
-    this._userLogged = this._userLogged$.subscribe(user => {
+    this._userLogged = this._userLoggedService.userLogged.subscribe(user => {
       if (!user.name) {
         this.title = 'Login';
         this.iconClass = 'bi bi-door-open';
