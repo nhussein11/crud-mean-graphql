b2bf49f605320bc6a621f00737158982d10f20c3
Nicolás Hussein <80422357+nhussein11@users.noreply.github.com>
2022-10-12 12:35:18 -0300

    fix: authenticate middleware

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,26 +1,13 @@
 const jwt = require("jsonwebtoken");
 
-// const authenticate = (req, res, next) => {
-//   try {
-//     const token = req.headers.authorization?.split(" ")[1] || '';
-//     const verified = jwt.verify(token, process.env.JWT_SECRET_KEY);
-//     req.verifiedUser = verified
-//     next();
-//   } catch (error) {
-//     console.log(error)
-//     throw Error("Invalid Token");
-//   }
-// };
-
 const authenticate = async (resolve, root, args, context, info) => {
   try {
-    const result = await resolve(root, args, context, info);
-    console.log(result)
-    const token = args.headers.authorization?.split(" ")[1] || "";
+
+    const token = context.headers.authorization?.split(" ")[1] || "";
     const verified = jwt.verify(token, process.env.JWT_SECRET_KEY);
-    req.verifiedUser = verified;
-    // next();
-    return result;
+    context.verifiedUser = verified;
+    return resolve(root, args, context, info);
+
   } catch (error) {
     throw Error("Invalid Token");
   }
